279116388e741ce9047e757389007d1d28e83038
Ahmet Bilal TARIM <ab_tarim@hotmail.com>
2024-07-17 00:22:09 +0300

    first refactoring of ValidateISBN

--- src/main/java/com/softesteam/isbnvalidator/ValidateISBN.java
+++ src/main/java/com/softesteam/isbnvalidator/ValidateISBN.java
@@ -2,23 +2,10 @@ package com.softesteam.isbnvalidator;
 
 public class ValidateISBN {
     public boolean checkISBN(String numberOfISBN) {
-        int[] digitsOfISBN = new int[10];
-        for (int i = 0; i < numberOfISBN.length(); i++) {
-            String s = numberOfISBN.substring(i, i + 1);
-            digitsOfISBN[i] = Integer.parseInt(s);
+        int total = 0;
+        for (int i = 0; i < 10; i++) {
+            total += numberOfISBN.charAt(i) * (10 - i);
         }
-        return isValidISBN(digitsOfISBN) == 0;
-    }
-
-    // Returns ISBN error syndrome, zero for a valid ISBN, non-zero for an invalid one.
-    // digits[i] must be between 0 and 10.
-    int isValidISBN(int[] digits) {
-        int i, s = 0, t = 0;
-
-        for (i = 0; i < 10; ++i) {
-            t += digits[i];
-            s += t;
-        }
-        return s % 11;
+        return total % 11 == 0;
     }
 }
