08aca4ed8253c53ec435845a7770df8311bdad34
Ahmet Bilal TARIM <ab_tarim@hotmail.com>
2024-07-16 22:29:04 +0300

    initian commit

---
+++ src/test/java/com/softesteam/isbnvalidator/IsbnValidatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softesteam.isbnvalidator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IsbnValidatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
